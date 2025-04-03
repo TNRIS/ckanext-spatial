0a4613853fef7fcedf39144c818c61cbd748fab6
rzmk <30333942+rzmk@users.noreply.github.com>
2025-04-03 13:27:13 -0400

    fix: run spatial query from spatial_simp field

--- ckanext/spatial/search/__init__.py
+++ ckanext/spatial/search/__init__.py
@@ -203,12 +203,12 @@ you need to split the geometry in order to fit the parts. Not indexing"""
         if not search_params.get("fq_list"):
             search_params["fq_list"] = []
 
-        default_spatial_query = "{{!field f=spatial_geom}}Intersects(ENVELOPE({minx}, {maxx}, {maxy}, {miny}))"
+        default_spatial_query = "{{!field f=spatial_simp}}Intersects(ENVELOPE({minx}, {maxx}, {maxy}, {miny}))"
 
         spatial_query = config.get("ckanext.spatial.solr_query", default_spatial_query)
 
         search_params["fq_list"].append(
-            spatial_query.format(spatial_field="spatial_geom", **bbox)
+            spatial_query.format(spatial_field="spatial_simp", **bbox)
         )
 
         return search_params

a1ba2e33f57245864574a550f0a1a4295e2c63f0
Ben Bright <ben.bright@twdb.texas.gov>
2026-03-23 13:41:30 -0500

    Rolling back commit ef86cc5a83df8a8d5e5d09abf28546f6d0919202

--- ckanext/spatial/public/js/spatial_query.js
+++ ckanext/spatial/public/js/spatial_query.js
@@ -191,7 +191,7 @@ this.ckan.module('spatial-query', function ($, _) {
         this.modal.on('shown.bs.modal', function () {
           if (module.drawMap) {
             module._setPreviousBBBox(map, zoom=false);
-            // map.fitBounds(module.mainMap.getBounds());
+            map.fitBounds(module.mainMap.getBounds());
 
             $('a.leaflet-draw-draw-rectangle>span', element).trigger('click');
             return
@@ -412,7 +412,7 @@ this.ckan.module('spatial-query', function ($, _) {
           
 
           module._setPreviousBBBox(map, zoom=false);
-          // map.fitBounds(module.mainMap.getBounds());
+          map.fitBounds(module.mainMap.getBounds());
 
           if (map.getZoom() == 0) {
             map.zoomIn();
